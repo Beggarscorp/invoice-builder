87f7c119613375751340a78eccd46abeed7b97ac
Beggarscorp <admin@beggarscorporation.com>
2026-04-30 13:29:24 +0530

    make debug page for debugging

---
+++ app/debug/pgae.tsx
@@ -0,0 +1,12 @@
+export const dynamic = 'force-dynamic';
+
+export default function DebugPage() {
+  return (
+    <div>
+      <p>DB_HOST: {process.env.DB_HOST}</p>
+      <p>DB_USER: {process.env.DB_USER}</p>
+      <p>DB_NAME: {process.env.DB_NAME}</p>
+      <p>DB_PORT: {process.env.DB_PORT}</p>
+    </div>
+  );
+}
