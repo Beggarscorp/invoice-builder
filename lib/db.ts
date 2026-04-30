7bb42127969fe54aaec75738d1dd695399adb124
Beggarscorp <admin@beggarscorporation.com>
2026-04-30 12:05:59 +0530

    add port in db connection page

--- lib/db.ts
+++ lib/db.ts
@@ -14,11 +14,12 @@ export async function connectDB() {
 
   try {
     const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
     return connection;
   } catch (err) {
     throw new Error(`DB connection failed: ${(err as Error).message}`);
